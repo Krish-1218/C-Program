d64315cf0e8079c722a782b0f69f5c83305e0a0e
Krish-1218 <ksbhise1812@gmail.com>
2025-06-25 21:30:07 +0530

    Create read_integer_value.c

---
+++ read_integer_value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int integer_num;
+    printf("enter the integer number ");
+    scanf("%d",&integer_num);
+
+    printf("The Integer number is : %d \n",integer_num);
+    return 0;
+}
