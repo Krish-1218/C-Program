287fe7557387ad9794836fde6d4ebdb15cd90624
Krish-1218 <ksbhise1812@gmail.com>
2025-07-06 19:33:56 +0530

    Create traingle.c

---
+++ traingle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+    float height,base,area;
+
+    printf("Height is ");
+    scanf("%f",&height);
+
+    printf("base is ");
+    scanf("%f",&base);
+
+   area = 0.5 *base * height;
+
+   printf("area of traingle is : %f",area);
+
+   return 0;
+
+}
