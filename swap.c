1e5085e96d9b6c7ed0e8cba915e804e9fded3941
Krish-1218 <ksbhise1812@gmail.com>
2025-07-06 19:33:49 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n1,n2;
+
+    printf("Enter values : ");
+    scanf("%d %d",&n1,&n2);
+
+    n1=n1+n2;
+    n2=n1-n2;
+    n1=n1-n2;
+
+    printf("After swapping n1= %d and n2=%d",n1,n2);
+
+    return 0;
+
+}
