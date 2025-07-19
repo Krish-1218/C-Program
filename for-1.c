275f604e0fd76e2420650249995e953b9474b6e7
Krish-1218 <ksbhise1812@gmail.com>
2025-07-19 22:33:53 +0530

    Update for-1.c

--- for-1.c
+++ for-1.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(){
-    int a;
+    int a,b;
 
     for (a=1; a<=5; a++)
     {
-       
-        printf("\n%d ", a);
+       for (b=1; b<=4; b++)
+       {
+           printf("  %d", b);
+       }
+       printf("\n%d", a);
     }
     
 }
