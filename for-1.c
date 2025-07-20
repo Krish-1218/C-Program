568d84b6e119ddd9f48573240f6d6873ef299a0b
Krish-1218 <ksbhise1812@gmail.com>
2025-07-20 18:32:19 +0530

    Update for-1.c

--- for-1.c
+++ for-1.c
@@ -2,9 +2,9 @@
 int main(){
     int a,b;
 
-    for (a=1; a<=5; a++)
+    for (a=1; a<=5; a++)  //loop for rows 5
     {
-       for (b=1; b<=4; b++)
+       for (b=1; b<=4; b++)// loop for columns 4
        {
            printf("  %d", b);
        }
