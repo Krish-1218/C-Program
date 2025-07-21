02511a2a0fd43a1e1a4b055e7788178ae0420594
Krish-1218 <ksbhise1812@gmail.com>
2025-07-21 19:50:20 +0530

    Create for-square-no-inline.c

---
+++ for-square-no-inline.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    for (int r=1; r<=5; r++)
+    {
+        for (int c=1; c<=5; c++)
+        {
+            printf("%d",r);
+        }
+        
+        printf("\n");
+    }
+    
+}
