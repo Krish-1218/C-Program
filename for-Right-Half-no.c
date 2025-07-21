9062760fc77a98419888a225174264de5ed9bdce
Krish-1218 <ksbhise1812@gmail.com>
2025-07-21 19:50:13 +0530

    Create for-Right-Half-no.c

---
+++ for-Right-Half-no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    for (int r=1; r<=5; r++)
+    {
+        for (int c=1; c<=r; c++)
+        {
+            printf("1");
+        }
+        
+        printf("\n");
+    }
+    
+}
