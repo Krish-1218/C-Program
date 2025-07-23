45f360b1493be1bc028649a9d40e5cace7c70e4b
Krish-1218 <ksbhise1812@gmail.com>
2025-07-23 12:21:46 +0530

    Update for-right-min-123--.c

--- for-right-min-123--.c
+++ for-right-min-123--.c
@@ -2,11 +2,17 @@
 
 int main(){
 
+    int a ,b=4;
+
     for (int r=1; r<=5; r++)
     {
+        a=r;
+        b=4;
         for (int c=1; c<=r; c++)
         {
-            printf("%d",r);
+            printf("%d",a);
+            a=a+b;
+            b--;
         }
         
         printf("\n");
