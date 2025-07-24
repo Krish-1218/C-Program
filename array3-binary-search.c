b1e7d4bad6bf9d2368d96f5f23848bcff0edc138
Krish-1218 <ksbhise1812@gmail.com>
2025-07-24 22:56:51 +0530

    Update array3-binary-search.c

---
+++ array3-binary-search.c
@@ -0,0 +1,52 @@
+//binary search -divide and con(means it divide first and find the midpoint for finding key )
+//sorted
+#include<stdio.h>
+int main(){
+
+    int length;
+    printf("Enter the Length of array :");
+    scanf("%d",&length);
+
+    int arr[length];
+
+    printf("Enter The Value Of Array :");
+    for (int i=0; i<length; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    length=sizeof(arr)/sizeof(arr[0]);
+
+    int key;
+    printf("Enter the Key :");
+    scanf("%d",&key);
+    
+    int start=0, end=length-1;
+    int index=-1;
+    while (start<=end)
+    {
+        int mid= (start+end)/2;
+        if (arr[mid]==key)
+        {
+            index=mid;
+            break;
+        }
+        else if (arr[mid]<=key)
+        {
+            start=mid+1;
+        }
+        else if (arr[mid]>=key)
+        {
+            end=mid-1;
+        }
+    }
+
+    if (index!=-1)
+    {
+        printf("Key Found At Index %d",index);
+    }
+    else{
+        printf("Key Not Found!");
+    }
+    
+    
+}
