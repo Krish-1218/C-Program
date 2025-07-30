8fe2eaa6f53a18e9c0086f958725556cfd4645c1
Krish-1218 <ksbhise1812@gmail.com>
2025-07-30 12:09:15 +0530

    Create string7-pali.c

---
+++ string7-pali.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+
+    char name[20];
+    printf("Enter Name: ");
+    scanf("%s", name);
+
+    int len;
+    for (len = 0; name[len]!='\0'; len++);
+    
+    for (int i = 0; i < len; i++) {
+        if (name[i] >= 'a' && name[i] <= 'z') 
+        {
+           name[i]=name[i]-32;
+        }
+    }
+    
+    int first=0, last=len-1, flag=1;
+   while (first<last)
+   {
+    if (name[first]!=name[last])
+    {
+        flag=0;
+        break;
+    }
+    first++;
+    last--;
+   }
+
+   printf(flag==1?"it's a pallindrome":"it's not a pallindrome");
+   
+}
